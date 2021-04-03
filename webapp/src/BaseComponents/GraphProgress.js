c92557f84f249d9b4d6bdc340449217725d5a7b1
prakharsaxena3322 <56475150+prakharsaxena3322@users.noreply.github.com>
2021-04-03 11:41:35 +0530

    Delete webapp directory

--- webapp/src/BaseComponents/GraphProgress.js
+++
@@ -1,8 +0,0 @@
-import CircularProgress from "@material-ui/core/CircularProgress";
-import React from 'react';
-import './GraphProgress.css';
-export default function GraphProgress() {
-    return (
-        <CircularProgress className="centered" disableShrink/>
-    );
-}
--- webapp/src/BaseComponents/UserId.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import TextField from '@material-ui/core/TextField';
-
-export default function userId(props) {
-    return (
-        <TextField id={'outlined-basic'} label={props.label}/>
-    );
-}
