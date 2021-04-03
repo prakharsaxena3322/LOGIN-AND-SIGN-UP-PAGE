fd73e864d124e76dc53b7d3e627fdef1d14fd388
prakharsaxena3322 <56475150+prakharsaxena3322@users.noreply.github.com>
2021-04-03 11:49:13 +0530

    Add files via upload

---
+++ test/signiner.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author ritik
+ */
+@WebServlet(urlPatterns = {"/signiner"})
+public class signiner extends HttpServlet {
+    static String email;
+    static String password;
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
+        String formname = request.getParameter("name");
+        String formemail = request.getParameter("email");
+        String formpassword1 = request.getParameter("password1");
+        String formpassword2 = request.getParameter("password2");
+        
+        if(!formemail.isEmpty() && !formpassword1.isEmpty() && !formname.isEmpty() && !formpassword2.isEmpty()){
+            if(formpassword1.equals(formpassword2)){
+                email = formemail;
+                password = formpassword1;
+                
+                RequestDispatcher req = request.getRequestDispatcher("welcome.jsp");
+		req.forward(request, response);
+            }
+        }
+        
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
